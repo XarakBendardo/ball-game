4c94c4cc730c556da21472d1d70b962e1f2252e5
Marcin <01169256@pw.edu.pl>
2024-06-02 22:45:45 +0200

    added getting executable direcory in a runtime

--- src/include/paths.h
+++ src/include/paths.h
@@ -1,11 +1,17 @@
 #include <string>
+#include <boost/dll/runtime_symbol_info.hpp>
 
 #ifndef PATHS_H
 #define PATHS_H
 
 namespace paths
 {
-    const std::string WORKING_DIR = ".";
+    static inline std::string getWorkingDir()
+    {
+        return boost::dll::program_location().parent_path().string();
+    }
+
+    const std::string WORKING_DIR = getWorkingDir();
     const std::string RESOURCES_DIR = WORKING_DIR + "/../resources";
     const std::string FONTS_DIR = RESOURCES_DIR + "/fonts";
     const std::string SOUNDS_DIR = RESOURCES_DIR + "/sounds";
